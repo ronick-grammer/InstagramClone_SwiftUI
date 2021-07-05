20b450df96cd2410b6f068525c82c8571e07ffc3
ronick-grammer <glflakcm@gmail.com>
2021-07-05 22:11:27 +0900

    Setup & Integrate Firebase

--- InstagramSwiftUI/InstagramSwiftUIApp.swift
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct InstagramSwiftUIApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    
+    
     var body: some Scene {
         WindowGroup {
 //            ContentView()
