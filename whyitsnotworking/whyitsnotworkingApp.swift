daec2c382a1e2736f0f06d1ad2e37505d950b809
ronick-grammer <glflakcm@gmail.com>
2021-07-13 20:14:42 +0900

    Initial Commit

---
+++ whyitsnotworking/whyitsnotworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  whyitsnotworkingApp.swift
+//  whyitsnotworking
+//
+//  Created by RONICK on 2021/07/13.
+//
+
+import SwiftUI
+
+@main
+struct whyitsnotworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
