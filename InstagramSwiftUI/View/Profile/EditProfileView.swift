3a97ed24fce532011560d1aada80e89269d2d93b
ronick-grammer <glflakcm@gmail.com>
2021-08-02 16:43:06 +0900

    Edit Profile UI

---
+++ InstagramSwiftUI/View/Profile/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  InstagramSwiftUI
+//
+//  Created by RONICK on 2021/08/02.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text("Edit Profile")
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
